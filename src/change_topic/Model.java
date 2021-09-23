c9617ec5b9c683151e68ea08876e897f2d5d31e8
Omkar Singh <omkars077@gmail.com>
2021-09-22 21:44:36 -0700

    model class

---
+++ src/change_topic/Model.java
@@ -0,0 +1,38 @@
+package change_topic;
+
+public class Model {
+
+    String name;
+    int age;
+    String subject;
+
+    public Model(String name, int age, String subject) {
+        this.name = name;
+        this.age = age;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
--- src/change_topic/MySubject.java
+++ src/change_topic/MySubject.java
@@ -1,25 +1,39 @@
-      package change_topic;
+package change_topic;
 
 
-      import java.util.ArrayList;
-      import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-      public class MySubject {
+public class MySubject {
+    public void student(){
 
-          public void details(){
+        List<Model> information = new ArrayList<> ();
 
-              List <Integer> age = new ArrayList<> ();
-              age.add(34);
-              age.add(22);
-              age.add(45);
-              age.add(29);
-              age.add(32);
-              for (Object var1 : age){
-                  System.out.println("This is all employees age information : "+var1);
-              }
-          }
-          public static void main(String[] args){
-              MySubject obj = new MySubject();
-              obj.details();
-          }
+        Model student1 = new Model("Surendra Kumar",26,"Commerce");
+        information.add(student1);
+
+        Model student2 = new Model("Manoj Kumar",29,"Computer");
+        information.add(student2);
+
+        Model student3 = new Model("Saurabh Sagar",25,"Science");
+        information.add(student3);
+
+        for (Model var1 : information){
+
+            System.out.println("This is details of all students : "+var1.getName());
+            System.out.println("This is all about of student age : "+var1.getAge());
+            System.out.println("This is all information of subject : "+var1.getSubject());
+
+        }
+    }
+    public static void main(String[] args){
+        MySubject obj = new MySubject();
+        obj.student();
+    }
+
+
+
+
+
+
 }
