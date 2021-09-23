3bd996b50640a698042c1b38b4e454786c815b2a
Omkar Singh <omkars077@gmail.com>
2021-09-22 21:19:00 -0700

    generics

--- src/change_topic/MySubject.java
+++ src/change_topic/MySubject.java
@@ -1,34 +1,25 @@
 package change_topic;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MySubject {
+    public void demo(){
+        List <String> subject = new ArrayList <String>();
+        subject.add("Economics");
+        subject.add("Commerce");
+        subject.add("English");
+        subject.add("Sociology");
 
-    public void demooflist(){
-
-        List student = new ArrayList();
-        student.add("Rahul Sharma");
-        student.add("Ajay Goyal");
-        student.add("Manoj Kumar");
-        student.add("Saurabh Sagar");
-        student.add("Rohit Sharma");
-
-        System.out.println("This is new admission : "+student.get(3));
-
-        for (Object var : student){
-            System.out.println("This is data of student : "+var);
-
+        for (Object var : subject){
+            System.out.println("This is all subject name details : "+var);
         }
-        student.remove(0);
-        student.remove(3);
-        for (Object var1 : student){
-            System.out.println("After deleting data : "+var1);
-        }
-
     }
     public static void main(String[] args){
         MySubject obj = new MySubject();
-        obj.demooflist();
+        obj.demo();
     }
+
+
 }
