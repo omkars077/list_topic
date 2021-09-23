a4a6d041458205d82c1f6a12e48e9f7adfeb497d
Omkar Singh <omkars077@gmail.com>
2021-09-22 21:26:26 -0700

    generics1

--- src/change_topic/MySubject.java
+++ src/change_topic/MySubject.java
@@ -1,25 +1,25 @@
-package change_topic;
+      package change_topic;
 
 
-import java.util.ArrayList;
-import java.util.List;
+      import java.util.ArrayList;
+      import java.util.List;
 
-public class MySubject {
-    public void demo(){
-        List <String> subject = new ArrayList <String>();
-        subject.add("Economics");
-        subject.add("Commerce");
-        subject.add("English");
-        subject.add("Sociology");
+      public class MySubject {
 
-        for (Object var : subject){
-            System.out.println("This is all subject name details : "+var);
-        }
-    }
-    public static void main(String[] args){
-        MySubject obj = new MySubject();
-        obj.demo();
-    }
+          public void details(){
 
-
+              List <Integer> age = new ArrayList<> ();
+              age.add(34);
+              age.add(22);
+              age.add(45);
+              age.add(29);
+              age.add(32);
+              for (Object var1 : age){
+                  System.out.println("This is all employees age information : "+var1);
+              }
+          }
+          public static void main(String[] args){
+              MySubject obj = new MySubject();
+              obj.details();
+          }
 }
